f81d877375c6d0b2b693d616173047befb8de353
Marykeller9 <marykeller9@gmail.com>
2023-06-25 14:46:08 -0700

    Lifo update

--- main.c
+++ main.c
@@ -1,77 +1,45 @@
 #include "monty.h"
-
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - function entry into interpreter
- * @argc: argc counter
- * @argv: arguments
- * Return: 0 on success
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	int file_d;
-	int ispushs = 0;
-	unsigned int lines = 1;
-	ssize_t n_read;
-	char *buff, *tkn;
-	stack_t *h = NULL;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file_d = open(argv[1], O_RDONLY);
-	if (file_d == -1)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	buff = malloc(sizeof(char) * 10000);
-	if (!buff)
-		return (0);
-	n_read = read(file_d, buff, 10000);
-	if (n_read == -1)
+	while (read_line > 0)
 	{
-		free(buff);
-		close(file_d);
-		exit(EXIT_FAILURE);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
-	tkn = strtok(buff, "\n\t\a\r ;:");
-	while (tkn != NULL)
-	{
-		if (ispushs == 1)
-		{
-			push(&h, lines, tkn);
-			ispushs = 0;
-			tkn = strtok(NULL, "\n\t\a\r ;:");
-			lines++;
-			continue;
-		}
-		else if (strcmp(tkn, "push") == 0)
-		{
-			ispushs = 1;
-			tkn = strtok(NULL, "\n\t\a\r ;:");
-			continue;
-		}
-		else
-		{
-			if (get_op_func(tkn) != 0)
-			{
-				get_op_func(tkn)(&h, lines);
-			}
-			else
-			{
-				free_dlist(&h);
-				printf("L%d: unknown instruction %s\n", lines, tkn);
-				exit(EXIT_FAILURE);
-			}
-		}
-		lines++;
-		tkn = strtok(NULL, "\n\t\a\r ;:");
-	}
-	free_dlist(&h);
-	free(buff);
-	close(file_d);
-	return (0);
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
